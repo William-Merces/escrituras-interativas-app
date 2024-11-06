3acce99181ea564531a93d23be907f8c27d698ef
William Mercês <engenheirowilliammerces@gmail.com>
2024-11-05 22:58:07 -0500

    fix: ajusta configuração e corrige importações para deploy no Vercel

---
+++ app/auth/Login/page.tsx
@@ -0,0 +1,9 @@
+// src/app/auth/login/page.tsx
+export default function LoginPage() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        {/* Conteúdo da página de login */}
+      </div>
+    );
+  }
